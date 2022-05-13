d86ac8cdfd71b150062f914de19489fb4dfc0a1d
Vladbor787 <borovik_nm@mail.ru>
2022-05-13 19:46:28 +0300

    the code improved. Now : function toTimeago returns values and doesn't print anything, some constants values moved to beginning of the program

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,10 @@ const val MINUTE_IN_SECONDS = 60
 const val SECONDS_IN_HOURS = 3600
 const val SECONDS_IN_DAY = 86400
 const val HOURS_IN_DAYS = 24
-
+val fraseMap = mapOf("justNow" to " только что", "yesterday" to " вчера",
+    " today" to " сегодня","ago" to " назад","long ago" to " давно",)
+val arrOfMinute = arrayOf("минуту", "минуты", "минут")
+val arrOfHours = arrayOf("час","часа","часов")
 
 fun main() {
 
@@ -11,30 +14,23 @@ fun main() {
         println("Введите количество секунд")
         val numberOfSeconds = readLine()?.toInt()
         if (numberOfSeconds != null && numberOfSeconds > 0) {
-            toTimeAgo(numberOfSeconds)
+           println("был(а) " + toTimeAgo(numberOfSeconds))
         }
     } while (numberOfSeconds != -1)
 
 }
-fun toTimeAgo(n: Int): String {
-
-    val fraseMap = mapOf("justNow" to " только что", "yesterday" to " вчера",
-        " today" to " сегодня","ago" to " назад","long ago" to " давно","was" to "был(а)")
-    val arrOfMinute = arrayOf("минуту", "минуты", "минут")
-    val arrOfHours = arrayOf("час","часа","часов")
+fun toTimeAgo(n: Int): String? {
 
     val numberOfHours = n / SECONDS_IN_HOURS % HOURS_IN_DAYS
     val numberOfMinutes = n / MINUTE_IN_SECONDS % MINUTE_IN_SECONDS
 
-
-
     return when(n) {
-       in 0..MINUTE_IN_SECONDS -> println(fraseMap["was"] + fraseMap["justNow"]).toString()
-       in MINUTE_IN_SECONDS + 1..SECONDS_IN_HOURS -> println(endingUtil(numberOfMinutes, arrOfMinute) + fraseMap["ago"]).toString()
-       in HOURS_IN_DAYS +1 ..SECONDS_IN_DAY -> println(endingUtil(numberOfHours, arrOfHours) + fraseMap["ago"]).toString()
-       in (SECONDS_IN_DAY +1)..(SECONDS_IN_DAY*2) -> println(fraseMap["was"] + fraseMap["today"]).toString()
-       in (SECONDS_IN_DAY*2)+1..SECONDS_IN_DAY*3-> println(fraseMap["was"] + fraseMap["yesterday"]).toString()
-       else -> println(fraseMap["was"] + fraseMap["long ago"]).toString()
+       in 0..MINUTE_IN_SECONDS -> (fraseMap["justNow"])
+       in MINUTE_IN_SECONDS + 1..SECONDS_IN_HOURS ->(endingUtil(numberOfMinutes, arrOfMinute) + fraseMap["ago"])
+       in HOURS_IN_DAYS +1 ..SECONDS_IN_DAY ->(endingUtil(numberOfHours, arrOfHours) + fraseMap["ago"])
+       in (SECONDS_IN_DAY +1)..(SECONDS_IN_DAY*2) ->(fraseMap["today"])
+       in (SECONDS_IN_DAY*2)+1..SECONDS_IN_DAY*3-> (fraseMap["yesterday"])
+       else -> (fraseMap["long ago"])
    }
 
 }
